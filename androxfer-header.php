16bd7668dca93ceb7bb79c547c8bb2b5d29f1283
Will DeBerry <willdeberry@gmail.com>
2014-07-01 18:21:45 +0200

    Remove lens flare as it caused the page to wrap and render improperly

--- androxfer-header.php
+++ androxfer-header.php
@@ -3,7 +3,7 @@
 		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 	<![endif]-->
 
-	<div class="lens-flare"></div>
+	<!--<div class="lens-flare"></div>-->
 	<div class="header-container">
 		<header class="wrapper clearfix">
 			<a href="http://andro.sudoservers.com">
