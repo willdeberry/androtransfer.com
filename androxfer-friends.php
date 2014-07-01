c19cebc4e75752d932b8fcee2e40d3ae72f1a5ae
Will DeBerry <willdeberry@gmail.com>
2014-07-01 18:02:25 +0200

    Spaces -> Tabs

--- androxfer-friends.php
+++ androxfer-friends.php
@@ -2,6 +2,6 @@
 		<div class="inner-partners">
 			<a rel="external" href="https://plus.google.com/u/0/communities/114009911819515408480"><i class="icon icon-pluscircles"></i>+AOKP</a>
 			<a rel="external" href="https://plus.google.com/u/0/112132608212015447534/posts"><i class="icon icon-pluscircles"></i>+SCAR45</a>
-            <a rel="external" href="https://plus.google.com/u/0/100014433524368062335/posts"><i class="icon icon-pluscircles"></i>+SIXSTRINGSG</a>
+			<a rel="external" href="https://plus.google.com/u/0/100014433524368062335/posts"><i class="icon icon-pluscircles"></i>+SIXSTRINGSG</a>
 			<a rel="external" href="http://www.webhostinghub.com/glyphs/"><i class="icon icon-webhostinghub"></i>WHH Glyph Fonts</a>
 		</div>
