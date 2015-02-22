60cb1fe85ab9044756eb323c36afbef8a8dfbcf3
Will DeBerry <willdeberry@gmail.com>
2015-02-22 02:53:47 +0100

    romlistings: Update json formatting that is returned from the cgi script

--- romlistings.py
+++ romlistings.py
@@ -6,97 +6,54 @@
 #
 # Usage Examples:
 #
-# Request all available information: http://xfer.aokp.co/romlistings.py?mode=all
-# Users available: http://xfer.aokp.co/romlistings.py
-# Devices available for specific User: http//xfer.aokp.co/romlistings.py?user=NAME
-# ROMs available for download: http//xfer.aokp.co/romlistings.py?user=NAME&device=HARDWARE
+# Request all available information: http://xfer.aokp.co/romlistings.py
 
-import cgi
 import json
 import os
 from datetime import datetime
 
-params = cgi.FieldStorage()
-mode = params.getvalue('mode')
-user = params.getvalue('user')
-device = params.getvalue('device')
+def listDevices(user):
+    return os.listdir(user)
 
-def getDevs():
-    data = []
-    with open(".users") as f:
-        contents = f.read().splitlines()
-        for dev in contents:
-            data.append({"name":dev})
+def listRoms(user,device):
+    return os.listdir("%s/%s" % (user, device))
 
-    print(json.dumps(data))
+def getUrl(user, device, rom):
+    return "http://xfer.aokp.co/%s/%s/%s" % (user, device, rom)
 
-def getDevices(user):
-    data = []
-    for f in os.listdir(user):
-        if not f.startswith('.'):
-            data.append({"devices":f})
 
-    print(json.dumps(data))
-
-def getRoms(user,device):
-    data = []
-    for f in os.listdir("%s/%s" % (user, device)):
-        if not f.startswith('.'):
-            url = "http://xfer.aokp.co/%s/%s/%s" % (user, device, f)
-            modified_date = os.path.getmtime("%s/%s/%s" % (user, device, f))
-            readable_date = datetime.fromtimestamp(modified_date).strftime("%Y-%m-%dT%H:%M:%S")
-            data.append({"date":readable_date,"url":url})
-
-    print(json.dumps(data))
-
-def showDevices(user):
-    devices = os.listdir(user)
-    return devices
-
-def showRoms(user,device):
-    rom = os.listdir("%s/%s" % (user, device))
-    return rom
-
-def showDetails(user,device,rom):
-    url = "http://xfer.aokp.co/%s/%s/%s" % (user, device, rom)
+def getDate(user, device, rom):
     modified_date = os.path.getmtime("%s/%s/%s" % (user, device, rom))
-    readable_date = datetime.fromtimestamp(modified_date).strftime("%Y-%m-%dT%H:%M:%S")
-    return url, readable_date
+    return datetime.fromtimestamp(modified_date).strftime("%Y-%m-%dT%H:%M:%S")
 
 def showAll():
     data = {}
-    with open(".users") as f:
-        users = f.read().splitlines()
-        for user in users:
-            data[user] = {}
-            for device in showDevices(user):
-                if not device.startswith('.'):
-                    if os.path.isdir("%s/%s" % (user, device)):
-                        data[user][device] = {}
-                        for rom in showRoms(user,device):
-                            if not rom.startswith('.'):
-                                if rom.endswith('zip'):
-                                    data[user][device][rom] = {}
-                                    u, d = showDetails(user, device, rom)
-                                    for url in u.splitlines():
-                                        data[user][device][rom]['url'] = url
-                                    for date in d.splitlines():
-                                        data[user][device][rom]['date'] = date
-
+    with open('.users') as users_file:
+        data['users'] = [
+            {
+                'name': user,
+                'devices': [
+                    {
+                        'codename': device,
+                        'roms': [
+                            {
+                                'filename': rom,
+                                'url': getUrl(user, device, rom),
+                                'date': getDate(user, device, rom)
+                            }
+                            for rom in listRoms(user, device) if not rom.startswith('.') if rom.endswith('zip')
+                        ]
+                    }
+                    for device in listDevices(user) if not device.startswith('.') if os.path.isdir("%s/%s" % (user, device))
+                ]
+            }
+            for user in users_file.read().splitlines()
+        ]
     print(json.dumps(data))
 
 def main():
     print("Content-type:application/json\n")
-    if mode == 'all':
-        showAll()
-    else:
-        if user:
-            if device:
-                getRoms(user,device)
-            else:
-                getDevices(user)
-        else:
-            getDevs()
+    showAll()
 
 if __name__ == "__main__":
      main()
