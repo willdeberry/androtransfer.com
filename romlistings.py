956a477300580c01fb2f80afc47c04191fe64abd
Justin Hall <justin.r.hall@gmail.com>
2015-02-25 10:09:26 -0700

    API: adhere to common Python style and code guidelines
    * Add docstrings
    * Wrap at 80 columns
    * Refactor class/function names
    * Fix indentation
    * Avoid cell-var-from-loop (Pylint)
    * Use print_function from __future__

--- romlistings.py
+++ romlistings.py
@@ -1,41 +1,70 @@
 #!/usr/bin/python
+"""
+This creates an API for xfer.AOKP.co
+Allows you to be able to poll for which users are available, which devices
+each user has, and what ROMs are available for download per device.
 
-# This creates an API for xfer.AOKP.co
-# Allows you to be able to poll for which users are available, which devices
-# each user has, and what ROMs are available for download per device.
-#
-# Usage Examples:
-#
-# Request all available information: http://xfer.aokp.co/romlistings.py
+Usage Examples:
 
+Request all available information: http://xfer.aokp.co/romlistings.py
+"""
+from __future__ import print_function
 import json
 import os
 from datetime import datetime
 from parse_config import parse_config
 
-class getDetails:
+
+class GetDetails(object):
+    """
+    Details class for a ROM associated with a device and user
+    """
     def __init__(self, user, device, rom):
         self.user = user
         self.device = device
         self.rom = rom
 
-    def getUrl(self):
-        return "http://xfer.aokp.co/%s/%s/%s" % (self.user, self.device, self.rom)
+    def get_url(self):
+        """
+        Return the URL string for this ROM
+        """
+        return "http://xfer.aokp.co/%s/%s/%s" % (self.user, self.device,
+                                                 self.rom)
 
-    def getDate(self):
-        modified_date = os.path.getmtime("%s/%s/%s" % (self.user, self.device, self.rom))
-        return datetime.fromtimestamp(modified_date).strftime("%Y-%m-%dT%H:%M:%S")
+    def get_date(self):
+        """
+        Return a nicely formatted last-modified string for this ROM
+        """
+        modified_date = os.path.getmtime("%s/%s/%s" % (self.user, self.device,
+                                                       self.rom))
+        return datetime.fromtimestamp(modified_date).strftime(
+            "%Y-%m-%dT%H:%M:%S")
 
-    def getSize(self):
+    def get_size(self):
+        """
+        Return the file size for this ROM
+        """
         return os.path.getsize("%s/%s/%s" % (self.user, self.device, self.rom))
 
-def listDevices(user):
+
+def list_devices(user):
+    """
+    Return a directory listing of devices for the specified user
+    """
     return os.listdir(user)
 
-def listRoms(user,device):
+
+def list_roms(user, device):
+    """
+    Return a directory listing of ROMs for the specified user and device
+    """
     return os.listdir("%s/%s" % (user, device))
 
-def getConfig(user, item):
+
+def get_config(user, item):
+    """
+    Return appropriate user info for a given item or None on failure
+    """
     try:
         user_info = parse_config('user_info/%s' % (user))
         return user_info[item]
@@ -43,41 +72,52 @@ def getConfig(user, item):
         pass
     except KeyError:
         pass
-
     return None
 
-def showAll():
+
+def show_all():
+    """
+    Return a JSON string containing all data
+    """
     data = {}
     with open('.users') as users_file:
         data['users'] = [
             {
                 'name': user,
-                'github': getConfig(user, 'github'),
-                'gravatar': getConfig(user, 'gravatar'),
-                'twitter': getConfig(user, 'twitter'),
+                'github': get_config(user, 'github'),
+                'gravatar': get_config(user, 'gravatar'),
+                'twitter': get_config(user, 'twitter'),
                 'devices': [
                     {
                         'codename': device,
                         'roms': [
-                            (lambda g: {
-                                'filename': rom,
-                                'url': g.getUrl(),
-                                'date': g.getDate(),
-                                'size': g.getSize()
-                            })(getDetails(user, device, rom))
-                            for rom in listRoms(user, device) if not rom.startswith('.') if rom.endswith('zip')
+                            (lambda g, this_rom=rom: {
+                                'filename': this_rom,
+                                'url': g.get_url(),
+                                'date': g.get_date(),
+                                'size': g.get_size()
+                            })(GetDetails(user, device, rom))
+                            for rom in list_roms(user, device)
+                            if not rom.startswith('.')
+                            if rom.endswith('zip')
                         ]
                     }
-                    for device in listDevices(user) if not device.startswith('.') if os.path.isdir("%s/%s" % (user, device))
+                    for device in list_devices(user)
+                    if not device.startswith('.')
+                    if os.path.isdir("%s/%s" % (user, device))
                 ]
             }
             for user in users_file.read().splitlines()
         ]
     return json.dumps(data)
 
+
 def main():
+    """
+    Main function, shows all data as JSON
+    """
     print("Content-type:application/json\n")
-    print(showAll())
+    print(show_all())
 
 if __name__ == "__main__":
-     main()
+    main()
